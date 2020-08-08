7736efc50ff06e769512350d09e8c51ac3f4f26f
rg <1xab@protonmail.com>
2020-08-08 22:59:16 +0200

    set-perma-commit.js: no-op on pull pages other than /commits

--- src/js/content-scripts/set-perma-commit-url.js
+++ src/js/content-scripts/set-perma-commit-url.js
@@ -1,27 +1,34 @@
-const commitURLPattern = /^\/(\w+)\/([A-Za-z0-9-_]+)\/pull\/\d+\/commits\/(\w+)$/;
+(function() {
+  const pullCommitURLPattern = /^\/(\w+)\/([A-Za-z0-9-_]+)\/pull\/\d+\/commits\/(\w+)$/;
+  const commitsURLPattern = /\/commits$/;
 
-const getPermaURL = (organization, repoName, commitSHA) => {
-  return `https://github.com/${organization}/${repoName}/commit/${commitSHA}`;
-};
+  const getPermaURL = (organization, repoName, commitSHA) => {
+    return `https://github.com/${organization}/${repoName}/commit/${commitSHA}`;
+  };
 
-const isPullCommitURL = (target) => {
-  return commitURLPattern.test(target.getAttribute('href'));
-};
+  const isPullCommitURL = (target) => {
+    return pullCommitURLPattern.test(target.getAttribute('href'));
+  };
 
-const setPermaURL = (links) => {
-  for(let i = 0; i < links.length; i++) {
-    const link = links[i];
-    if(isPullCommitURL(link)) {
-      link.setAttribute('href', getPermaURL(RegExp.$1, RegExp.$2, RegExp.$3))
+  const setPermaURL = (links) => {
+    for (let i = 0; i < links.length; i++) {
+      const link = links[i];
+      if (isPullCommitURL(link)) {
+        link.setAttribute('href', getPermaURL(RegExp.$1, RegExp.$2, RegExp.$3))
+      }
     }
-  }
-};
+  };
 
-const getNodes = () => {
-  return document.querySelectorAll('.TimelineItem-body li a');
-};
+  const getNodes = () => {
+    if (commitsURLPattern.test(location.href)) {
+      return document.querySelectorAll('.TimelineItem-body li a');
+    } else {
+      return [];
+    }
+  };
 
-document.addEventListener('DOMContentLoaded', () => {
-  setInterval(() => setPermaURL(getNodes(), 1000));
-  setPermaURL(getNodes());
-});
+  document.addEventListener('DOMContentLoaded', () => {
+    setInterval(() => setPermaURL(getNodes(), 1000));
+    setPermaURL(getNodes());
+  });
+})();
