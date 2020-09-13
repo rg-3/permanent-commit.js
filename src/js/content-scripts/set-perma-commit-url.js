a79d40c51c986de6ae20fd39adb1a52b4f6fc76f
rg <1aab@protonmail.com>
2020-09-13 23:33:16 +0200

    Rewrite URLs to permanent URLs on discussion page

--- src/js/content-scripts/set-perma-commit-url.js
+++ src/js/content-scripts/set-perma-commit-url.js
@@ -1,6 +1,6 @@
 (function() {
   const pullCommitURLPattern = /^\/([A-Za-z0-9-_.]+)\/([A-Za-z0-9-_.]+)\/pull\/\d+\/commits\/([a-z0-9]+)$/;
-  const commitsURLPattern    = /\/commits$/;
+  const commitsURLPattern    = /(\/pull\/\d+|\/commits)$/;
 
   const getPermaURL = (username, repo, sha) => {
     return `https://github.com/${username}/${repo}/commit/${sha}`;
@@ -18,7 +18,7 @@
 
   const getNodes = () => {
     if (commitsURLPattern.test(location.href)) {
-      const sel = '#commits_bucket .TimelineItem li a.js-navigation-open';
+      const sel = '#commits_bucket .TimelineItem li a.js-navigation-open, #discussion_bucket .commit-message a';
       return document.querySelectorAll(sel);
     } else {
       return [];
